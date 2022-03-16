648be3a978858b6fbb49e1c9c41480356004190e
jaisas-jessy18 <jaisas.jessy3@gmail.com>
2022-03-16 19:02:49 +0530

    created components and added routing

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomeComponent } from './home/home/home.component';
-import { LoginComponent } from './login/login/login.component';
+import { CartComponent } from './cart/cart.component';
+import { CatalogueComponent } from './catalogue/catalogue.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { PosComponent } from './pos/pos.component';
+import { SalesComponent } from './sales/sales.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'cart', component: CartComponent },
+  { path: 'catalogue', component: CatalogueComponent },
+  { path: 'sales', component: SalesComponent },
+  { path: 'pos', component: PosComponent },
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,22 +1,30 @@
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login/login.component';
-import { HomeComponent } from './home/home/home.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeaderComponent } from './header/header.component';
+import { CartComponent } from './cart/cart.component';
+import { SalesComponent } from './sales/sales.component';
+import { PosComponent } from './pos/pos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    HomeComponent,
     LoginComponent,
-    HomeComponent
+    DashboardComponent,
+    HeaderComponent,
+    CartComponent,
+    SalesComponent,
+    PosComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
